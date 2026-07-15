a86bee01066fc98b15c1dc5124e249d2a1ee8d6c
morefreeze <morefreeze@gmail.com>
2026-07-15 16:50:40 +0800

    feat: resolve mainstream video feed URLs

--- backend/internal/rss/resolver_test.go
+++ backend/internal/rss/resolver_test.go
@@ -2,31 +2,32 @@ package rss
 
 import "testing"
 
-func TestResolveFeedURL(t *testing.T) {
-	cases := []struct {
-		name       string
-		in         string
-		rsshubBase string
-		want       string
-	}{
-		{"bilibili_user_root", "https://space.bilibili.com/14064034", "http://rsshub:1200", "http://rsshub:1200/bilibili/user/video/14064034"},
-		{"bilibili_user_video", "https://space.bilibili.com/14064034/video", "http://rsshub:1200", "http://rsshub:1200/bilibili/user/video/14064034"},
-		{"bilibili_user_video_slash", "https://space.bilibili.com/14064034/video/", "http://rsshub:1200", "http://rsshub:1200/bilibili/user/video/14064034"},
-		{"bilibili_user_trailing_slash", "https://space.bilibili.com/14064034/", "http://rsshub:1200", "http://rsshub:1200/bilibili/user/video/14064034"},
-		{"trims_trailing_slash_on_base", "https://space.bilibili.com/14064034", "http://rsshub:1200/", "http://rsshub:1200/bilibili/user/video/14064034"},
-		{"www_prefix", "https://www.space.bilibili.com/14064034", "http://rsshub:1200", "http://rsshub:1200/bilibili/user/video/14064034"},
-		{"non_numeric_uid", "https://space.bilibili.com/abc", "http://rsshub:1200", "https://space.bilibili.com/abc"},
-		{"deep_path_unsupported", "https://space.bilibili.com/14064034/dynamic", "http://rsshub:1200", "https://space.bilibili.com/14064034/dynamic"},
-		{"different_host_passthrough", "https://www.youtube.com/watch?v=abc", "http://rsshub:1200", "https://www.youtube.com/watch?v=abc"},
-		{"empty_base_passthrough", "https://space.bilibili.com/14064034", "", "https://space.bilibili.com/14064034"},
-		{"empty_input", "", "http://rsshub:1200", ""},
-		{"malformed_input", "://nope", "http://rsshub:1200", "://nope"},
-	}
+type resolveCase struct {
+	name       string
+	input      string
+	rsshubBase string
+	want       string
+}
+
+func runResolveCases(t *testing.T, cases []resolveCase) {
+	t.Helper()
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			if got := ResolveFeedURL(tc.in, tc.rsshubBase); got != tc.want {
-				t.Errorf("ResolveFeedURL(%q, %q) = %q, want %q", tc.in, tc.rsshubBase, got, tc.want)
+			if got := ResolveFeedURL(tc.input, tc.rsshubBase); got != tc.want {
+				t.Fatalf("ResolveFeedURL(%q, %q) = %q, want %q", tc.input, tc.rsshubBase, got, tc.want)
 			}
 		})
 	}
+}
+
+func TestResolveFeedURLCore(t *testing.T) {
+	runResolveCases(t, []resolveCase{
+		{name: "empty_base", input: "https://www.youtube.com/channel/UC123", rsshubBase: "", want: "https://www.youtube.com/channel/UC123"},
+		{name: "empty_input", input: "", rsshubBase: "http://rsshub:1200", want: ""},
+		{name: "malformed_input", input: "://nope", rsshubBase: "http://rsshub:1200", want: "://nope"},
+		{name: "relative_input", input: "/user/123", rsshubBase: "http://rsshub:1200", want: "/user/123"},
+		{name: "non_http_scheme", input: "ftp://space.bilibili.com/14064034", rsshubBase: "http://rsshub:1200", want: "ftp://space.bilibili.com/14064034"},
+		{name: "existing_rsshub_url", input: "http://rsshub:1200/weibo/user/1195230310", rsshubBase: "http://rsshub:1200/", want: "http://rsshub:1200/weibo/user/1195230310"},
+		{name: "unmatched_host", input: "https://example.com/blog", rsshubBase: "http://rsshub:1200", want: "https://example.com/blog"},
+	})
 }
---
+++ backend/internal/rss/resolver_video_test.go
@@ -0,0 +1,32 @@
+package rss
+
+import "testing"
+
+func TestResolveFeedURLVideoPlatforms(t *testing.T) {
+	const base = "http://rsshub:1200"
+	runResolveCases(t, []resolveCase{
+		{name: "bilibili_user", input: "https://space.bilibili.com/14064034", rsshubBase: base, want: base + "/bilibili/user/video/14064034"},
+		{name: "bilibili_user_video", input: "https://www.space.bilibili.com/14064034/video/?from=search#top", rsshubBase: base, want: base + "/bilibili/user/video/14064034"},
+		{name: "bilibili_non_numeric", input: "https://space.bilibili.com/not-a-uid", rsshubBase: base, want: "https://space.bilibili.com/not-a-uid"},
+		{name: "bilibili_dynamic", input: "https://space.bilibili.com/14064034/dynamic", rsshubBase: base, want: "https://space.bilibili.com/14064034/dynamic"},
+
+		{name: "youtube_channel_native", input: "https://www.youtube.com/channel/UCsXVk37bltHxD1rDPwtNM8Q/videos", rsshubBase: base, want: "https://www.youtube.com/feeds/videos.xml?channel_id=UCsXVk37bltHxD1rDPwtNM8Q"},
+		{name: "youtube_playlist_native", input: "https://youtube.com/playlist?list=PL123&utm_source=test", rsshubBase: base, want: "https://www.youtube.com/feeds/videos.xml?playlist_id=PL123"},
+		{name: "youtube_handle", input: "https://youtube.com/@Fireship/videos?view=0", rsshubBase: base, want: base + "/youtube/user/@Fireship"},
+		{name: "youtube_user", input: "https://m.youtube.com/user/GoogleDevelopers", rsshubBase: base, want: base + "/youtube/user/GoogleDevelopers"},
+		{name: "youtube_custom", input: "https://www.youtube.com/c/Computerphile", rsshubBase: base, want: base + "/youtube/c/Computerphile"},
+		{name: "youtube_watch_passthrough", input: "https://www.youtube.com/watch?v=abc", rsshubBase: base, want: "https://www.youtube.com/watch?v=abc"},
+		{name: "youtube_shorts_passthrough", input: "https://www.youtube.com/shorts/abc", rsshubBase: base, want: "https://www.youtube.com/shorts/abc"},
+		{name: "youtube_playlist_missing_id", input: "https://www.youtube.com/playlist", rsshubBase: base, want: "https://www.youtube.com/playlist"},
+
+		{name: "douyin_user", input: "https://www.douyin.com/user/MS4wLjABAAAAexample?from_tab_name=main", rsshubBase: base, want: base + "/douyin/user/MS4wLjABAAAAexample"},
+		{name: "douyin_hashtag", input: "https://www.douyin.com/hashtag/123456", rsshubBase: base, want: base + "/douyin/hashtag/123456"},
+		{name: "douyin_live", input: "https://live.douyin.com/987654", rsshubBase: base, want: base + "/douyin/live/987654"},
+		{name: "douyin_video_passthrough", input: "https://www.douyin.com/video/123", rsshubBase: base, want: "https://www.douyin.com/video/123"},
+		{name: "douyin_invalid_user", input: "https://www.douyin.com/user/short-id", rsshubBase: base, want: "https://www.douyin.com/user/short-id"},
+
+		{name: "tiktok_user", input: "https://www.tiktok.com/@linustech/", rsshubBase: base, want: base + "/tiktok/user/@linustech"},
+		{name: "tiktok_video_passthrough", input: "https://www.tiktok.com/@linustech/video/123", rsshubBase: base, want: "https://www.tiktok.com/@linustech/video/123"},
+		{name: "tiktok_live_passthrough", input: "https://www.tiktok.com/@linustech/live", rsshubBase: base, want: "https://www.tiktok.com/@linustech/live"},
+	})
+}
